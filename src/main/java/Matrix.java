450f6f8980d52e49293d0cd96c044f30acb1f076
Evgeny Zheleznov <zhel186@mail.ru>
2021-10-28 17:22:04 +0300

    added changes Matrix

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -22,6 +22,7 @@ public class Matrix {
         System.out.println("-----------------");
 
 
+        System.out.println("Ниже главной диагонали");
         for (int i = maxInd; i >= 0; i--) {
             for (int j = i - 1; j >= 0; j--) {
                 System.out.print(matrix[i][j] + "\t");
@@ -29,7 +30,7 @@ public class Matrix {
             System.out.println();
         }
 
-        System.out.println("---------");
+        System.out.println("Выше главной диагонали");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = i + 1; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + "\t");
@@ -37,7 +38,8 @@ public class Matrix {
             System.out.println();
         }
 
-        for (int i = maxInd; i >= 0; i--) {
+        System.out.println("Выше побочной диагонали");
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = maxInd - i - 1; j >= 0; j--) {
                 System.out.print(matrix[i][j] + "\t");
             }
@@ -45,6 +47,14 @@ public class Matrix {
         }
 
 
+
+        System.out.println("Ниже побочной диагонали");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = matrix[i].length - i; j <matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
 
     }
 }
