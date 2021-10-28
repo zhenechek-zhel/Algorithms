65c6d067cf585e361aefadaa41209a26fa67c050
Evgeny Zheleznov <zhel186@mail.ru>
2021-10-28 14:49:00 +0300

    added tree and InsertSort

---
+++ src/main/java/InsertSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{2, 7, 5, 3, 9, 1, 6, 4, 8, 11, 12, 10};
+        insertSorting(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+   private static void insertSorting(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > current) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = current;
+        }
+   }
+}
